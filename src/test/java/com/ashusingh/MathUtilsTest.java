1b19ccef0901340b57bf0da84acd48a98a132a74
Ashu Singh <ashu.ask.singh@gmail.com>
2022-01-11 02:26:07 +0530

    Writing Tests with Output messages

--- src/test/java/com/ashusingh/MathUtilsTest.java
+++ src/test/java/com/ashusingh/MathUtilsTest.java
@@ -9,8 +9,17 @@ class MathUtilsTest {
     @Test
     void test() {
         MathUtils mathUtils = new MathUtils();
-        int expected = 1;
+        int expected = 2;
         int actual = mathUtils.add(1,1);
         assertEquals(expected, actual, "Add method Failed");
     }
+
+    @Test
+    void computeCircleArea(){
+        MathUtils mathUtils = new MathUtils();
+        assertEquals(Math.PI*10*10, mathUtils.computeCircleArea(10),
+                "Should return right circle area");
+
+    }
+
 }
